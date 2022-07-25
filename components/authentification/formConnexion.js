5441dae59829d53922eaa27cd8c20510c64c658a
Antoine AMILIEN <antoine.amilien@nxo.eu>
2022-07-25 15:58:50 +0200

    [FEAT] Nextauth base

--- components/authentification/formConnexion.js
+++ components/authentification/formConnexion.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signIn } from "next-auth/react"
 
-const formConnexion = () => {
+
+
+
+const formConnexion = (props) => {
+
+    const { errorMessage } = props;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email == "")
+            // "username-login" matches the id for the credential
+            signIn("username-login", { email, password });
+    };
+
     return (
         <div>
-            <form action="#" method="POST" class="space-y-4">
+            <form onSubmit={handleSubmit} class="space-y-4">
 
                 <div class="form-control w-full">
                     <label class="label">
                         <span class="label-text">Email</span>
                     </label>
-                    <input type="text" placeholder="Email" class="input input-bordered w-full" />
+                    <input type="text" id="email" name="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" class="input input-bordered w-full" />
                 </div>
 
                 <div class="form-control w-full">
                     <label class="label">
                         <span class="label-text">Mot de passe</span>
                     </label>
-                    <input type="password" placeholder="Mot de passe" class="input input-bordered w-full" />
+                    <input id="password" name="password" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Mot de passe" class="input input-bordered w-full" />
                 </div>
 
+                <p className="font-medium text-red-500"> {errorMessage} </p>
 
                 <button class="btn btn-primary w-full">Connexion</button>
+
             </form>
         </div>
     );
 };
+
+
+
+
 
 export default formConnexion;
